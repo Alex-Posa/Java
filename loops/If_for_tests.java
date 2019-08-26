7db3cd79061a8b0d615ab38f6a66e20ef7e2fe70
Alex-Posa <50545422+Alex-Posa@users.noreply.github.com>
2019-08-26 09:27:42 +0200

    Create If_for_tests.java

---
+++ loops/If_for_tests.java
@@ -0,0 +1,19 @@
+public class IF_loops {
+
+	public static void main(String[] args) {
+	int x = 10;
+	
+	if(x > 0) {
+		for(x = 10; x > 0; x--) {
+			System.out.println(x);
+		}
+		
+		}
+	System.out.println("Loop ended");
+	}
+}
+
+
+
+
+// Just testing the if and for methods, nothing to see here.
