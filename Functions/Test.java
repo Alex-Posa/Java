7011ba6ea329a1806f5f0b62c122272280f23e60
Alex-Posa <50545422+Alex-Posa@users.noreply.github.com>
2019-08-27 13:07:38 +0200

    Create Test.java

---
+++ Functions/Test.java
@@ -0,0 +1,15 @@
+package functions;
+
+public class Main {
+	public static void main(String[] args) {
+		int x = 4;
+		addOneto(x);
+		System.out.println(x);
+	}
+	static int addOneto(int num) {
+		
+		num += 1;
+		return num;
+	}
+	
+}
