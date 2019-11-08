544074ec6b54c46bc03a75736344dad27f05a6af
Alex-Posa <50545422+Alex-Posa@users.noreply.github.com>
2019-11-08 10:45:16 +0100

    Create Kurse_class.java

---
+++ PG2/Aufgabe_5/Studentenverwaltung/Kurse_class.java
@@ -0,0 +1,24 @@
+package studentenverwaltung;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Kurse {
+
+	private String kursName;
+	
+	private int credits;
+	
+	private int multiplikator;
+	
+	private double note;
+	
+	private List<String> kurse = new ArrayList<String>();
+
+
+
+
+
+
+
+}
