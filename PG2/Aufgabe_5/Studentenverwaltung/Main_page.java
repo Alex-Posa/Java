a77ad91212e5f56b5472708c61f5c21750e00dbb
Alex-Posa <50545422+Alex-Posa@users.noreply.github.com>
2019-11-08 10:44:05 +0100

    Create Main_page.java

---
+++ PG2/Aufgabe_5/Studentenverwaltung/Main_page.java
@@ -0,0 +1,19 @@
+package studentenverwaltung;
+
+
+public class Main_programm {
+
+	public static void main(String[] args) {
+	
+		Studiengaenge s = new Studiengaenge();
+		Hochschule hs = new Hochschule("sadas", s);
+	
+		
+		
+		Student newStudent = new Student();
+		newStudent.setVorName("jhagshjafgkdsfg");
+		
+	
+	}
+
+}
