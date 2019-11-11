9de927db915dabb5d4166e16de5355f671c0f587
Alex-Posa <50545422+Alex-Posa@users.noreply.github.com>
2019-11-11 15:03:13 +0100

    Create Farbe.java

---
+++ PG2/Aufgabe_5/Artikelverwaltung/Farbe.java
@@ -0,0 +1,32 @@
+package artikelverwaltung;
+
+public class Farbe {
+
+	private int rot;
+	private int blau;
+	private int grün;
+	
+	
+	
+	public int getRot() {
+		return rot;
+	}
+	public void setRot(int rot) {
+		this.rot = rot;
+	}
+	public int getBlau() {
+		return blau;
+	}
+	public void setBlau(int blau) {
+		this.blau = blau;
+	}
+	public int getGrün() {
+		return grün;
+	}
+	public void setGrün(int grün) {
+		this.grün = grün;
+	}
+	
+	
+	
+}
