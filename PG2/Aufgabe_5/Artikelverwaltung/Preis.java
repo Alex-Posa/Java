45fd4860a9f82a8cd20eee663dffd8aa873b67c9
Alex-Posa <50545422+Alex-Posa@users.noreply.github.com>
2019-11-11 15:03:53 +0100

    Create Preis.java

---
+++ PG2/Aufgabe_5/Artikelverwaltung/Preis.java
@@ -0,0 +1,25 @@
+package artikelverwaltung;
+
+public class Preis {
+	
+	private String währung;
+	private double wert;
+	
+	
+	public String getWährung() {
+		return währung;
+	}
+	public void setWährung(String währung) {
+		this.währung = währung;
+	}
+	public double getWert() {
+		return wert;
+	}
+	public void setWert(double wert) {
+		this.wert = wert;
+	}
+	
+	
+	
+	
+}
