23c1ec49b5afb3ff8ce9daf9ec8be1e0a0a02c1d
Alex-Posa <50545422+Alex-Posa@users.noreply.github.com>
2019-11-18 13:40:23 +0100

    Update Studiengaenge_class.java

--- PG2/Aufgabe_5/Studentenverwaltung/Studiengaenge_class.java
+++ PG2/Aufgabe_5/Studentenverwaltung/Studiengaenge_class.java
@@ -1,16 +1,24 @@
 package studentenverwaltung;
 
-public class Studiengaenge {
+public class Studiengänge {
+	private String name;
+	private Kurse krs;
+	
+	//Getters and Setters
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Kurse getKrs() {
+		return krs;
+	}
+	public void setKrs(Kurse krs) {
+		this.krs = krs;
+	}
+	
+	//Logic
 
-	private String studiengang;
-	
-		private String studiengangName;
-	
-	
-	
-	
-	
-	
-	
-	
 }
