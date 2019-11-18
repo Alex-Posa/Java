a8bdb8279bae1a95db168f57c52bca4e301dd00a
Alex-Posa <50545422+Alex-Posa@users.noreply.github.com>
2019-11-18 13:39:39 +0100

    Update Main_page.java

--- PG2/Aufgabe_5/Studentenverwaltung/Main_page.java
+++ PG2/Aufgabe_5/Studentenverwaltung/Main_page.java
@@ -1,19 +1,61 @@
 package studentenverwaltung;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class Main_programm {
+
+public class Main {
 
 	public static void main(String[] args) {
-	
-		Studiengaenge s = new Studiengaenge();
-		Hochschule hs = new Hochschule("sadas", s);
-	
+		
+		List<Student> studentList = new ArrayList<Student>();
+		
+		Hochschule othr = new Hochschule();
+		othr.setName("OTH Regensburg");
+
 		
 		
-		Student newStudent = new Student();
-		newStudent.setVorName("jhagshjafgkdsfg");
+		//add students to your hearts extent
 		
-	
+		Student alex = new Student();
+		alex.setName("Alex");
+		alex.setMatrikelnummer(30008600);
+		alex.setEmail("alex@email.de");
+		studentList.add(alex);
+		
+		Student chris = new Student();
+		chris.setName("Chris");
+		chris.setMatrikelnummer(40004300);
+		chris.setEmail("chris@email.de");
+		studentList.add(chris);
+		
+		
+		//Studiengänge
+		
+		Studiengänge informatik1 = new Studiengänge();
+		informatik1.setName("allgemeine Informatik");
+
+		
+		
+		//Kurse
+		
+		Kurse mathe = new Kurse();
+		mathe.setName("Mathematik 1");
+		mathe.setMultiplikator(0.5);
+		mathe.setCredits(7);
+		
+		Kurse programmieren = new Kurse();
+		programmieren.setName("Programmieren 1");
+		programmieren.setMultiplikator(0.5);
+		programmieren.setCredits(8);
+		
+		// Logic stuff
+		
+		
+		
+		Student.printList(studentList);
+		
+		
 	}
 
 }
