4c8b8c3089ea61e6d8eb6fe551b53da7a5526803
Alex-Posa <50545422+Alex-Posa@users.noreply.github.com>
2019-11-26 14:18:16 +0100

    Create Programm.java

---
+++ PG2/Aufgabe_6/Static_Student/Programm.java
@@ -0,0 +1,12 @@
+package static_student;
+
+public class Programm {
+
+	public static void main(String[] args) {
+		Student tomStudent = new Student(1,"Tom", "Teuer");
+		Student reginaStudent = new Student(2,"Regina", "Reich");
+		Student leaStudent = new Student(3,"Lea", "Lustig");
+		
+	}
+
+}
