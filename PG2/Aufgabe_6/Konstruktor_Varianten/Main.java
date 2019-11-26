b76c2e4d381f185ac2cdb222df084ee38ae7ffed
Alex-Posa <50545422+Alex-Posa@users.noreply.github.com>
2019-11-26 11:13:22 +0100

    Create Main.java

---
+++ PG2/Aufgabe_6/Konstruktor_Varianten/Main.java
@@ -0,0 +1,14 @@
+package konstruktor_varianten;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Person one   = new Person();
+		Person two   = new Person("Alex");
+		Person three = new Person("Alex", "Posa");
+		
+		
+	}
+
+}
