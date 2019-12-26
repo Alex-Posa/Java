095a1c13b989ca52a798f902a3aa3082361dae85
Alexander Posamanov <50545422+Alex-Posa@users.noreply.github.com>
2019-12-26 16:38:24 +0100

    Create Amsel.java

---
+++ PG2/Aufgabe_7/7.1_Vogelprogramm/Amsel.java
@@ -0,0 +1,19 @@
+package vogelprogramm;
+
+public class Amsel extends Vogel implements KannFliegen {
+
+	public Amsel(String name) {
+		super(name);
+	}
+
+	@Override
+	public void fliegen() {
+		
+	}
+
+	@Override
+	public void singen() {
+		
+	}
+	
+}
