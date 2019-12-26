237b1cb261b7e0e4e84b4ec6b4deff23705a0556
Alexander Posamanov <50545422+Alex-Posa@users.noreply.github.com>
2019-12-26 16:38:04 +0100

    Create Drossel.java

---
+++ PG2/Aufgabe_7/7.1_Vogelprogramm/Drossel.java
@@ -0,0 +1,19 @@
+package vogelprogramm;
+
+public class Drossel extends Vogel implements KannFliegen {
+	
+	public Drossel(String name) {
+		super(name);
+	}
+
+	@Override
+	public void fliegen() {
+		
+	}
+
+	@Override
+	public void singen() {
+		
+	}
+	
+}
